5bd0893aba3ee6d166f35f703f456036669f41ce
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-13 08:51:51 +0530

    IsNotEmpty validation added

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -4,35 +4,44 @@ import {
 } from 'class-validator';
 
 export class CreateTaskDto {
-  
+    @IsNotEmpty()
     @IsString()
     name: string;
- 
+
+    @IsNotEmpty()
     @IsString()
     description: string;
   
+    @IsNotEmpty()
     @IsString()
     dueDate: string;
-  
+    
+    @IsNotEmpty()
     @IsBoolean()
     completed: boolean
 }
 
 export class TaskUpdatesDto {
+    @IsNotEmpty()
     @IsOptional()
     @IsBoolean()
     completed?: boolean;
 }
 
 export class UpdateTaskDto {
+    
+    @IsNotEmpty()
     @IsString()
     name: string;
     
+    @IsNotEmpty()
     @IsString()
     description: string;
     
+    @IsNotEmpty()
     @IsString()
     dueDate: string;
+    @IsNotEmpty()
 
     @IsOptional()
     @IsBoolean()
@@ -40,6 +49,7 @@ export class UpdateTaskDto {
 }
 
 export class DeleteTaskDto {
+    @IsNotEmpty()
     @IsString()
     name: string;
 }
