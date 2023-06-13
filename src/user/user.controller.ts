9f654d518ec81e7f1a21f5f4e948b91508adf85a
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-13 08:45:18 +0530

    async meathod, find the user by username

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller,Post} from '@nestjs/common';
-import { CheckUserDto } from './dto/user.dto';
+import { Body, Controller,Get,Post} from '@nestjs/common';
+import { CheckUserDto, IsUserDto, IsUserFail } from './dto/user.dto';
 import { UserService } from './user.service';
+import { async } from 'rxjs';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService:UserService){}
 
+    @Post()
+    getUserDetails(@Body() checkUserDto:CheckUserDto){
+        return this.userService.findOnes(checkUserDto)
+    }
 
     @Post('login')
-    validateUser(@Body() checkUserDto:CheckUserDto){
-        return this.userService.findOneOnly(checkUserDto)
+    async validateUser(@Body() checkUserDto:CheckUserDto):Promise<IsUserFail>{
+        const users = await this.userService.findOnes(checkUserDto)
+        // console.log(users);
+        // is user exits { success: true } if null false
+        if (users) {
+            return { success: true };
+          } else {
+            return { success: false };
+          }
     }
+
 }
