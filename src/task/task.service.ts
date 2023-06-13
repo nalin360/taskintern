54764032d3594c1b713fa86c027421ab68ab5ad7
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-13 08:56:29 +0530

    TaskUpdateDto => UpdateTAskDto

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { TaskUpdatesDto , CreateTaskDto } from './dto/task.dto';
+import { TaskUpdatesDto , CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
 
 
 @Injectable()
@@ -19,7 +19,7 @@ export class TaskService {
         return taskCreated.save();
     }
 
-    async updateTask(name:string , updatetask: TaskUpdatesDto) {
+    async updateTask(name:string , updatetask: UpdateTaskDto) {
         // findOneAndUpdate(filter, update); 
         // {new: true } return the document after update was applied
         return this.taskModel.findOneAndUpdate({name} ,updatetask,{ new: true })
