a1f729f150e516c4611a40c5d346aa3fe8979856
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-15 00:58:17 +0530

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,16 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { TaskService } from './task/task.service';
+import { T1Controller } from './t1/t1.controller';
+import { T1Service } from './t1/t1.service';
+import { T2Controller } from './t2/t2.controller';
+import { T2Module } from './t2/t2.module';
 
 
 @Module({
-  imports: [TaskModule , MongooseModule.forRoot('mongodb://127.0.0.1:27017/task'), UserModule],
+  imports: [TaskModule , 
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/task'), 
+    UserModule,T2Module],
   controllers: [AppController],
   providers: [AppService],
 })
