238225eb4c789ae8fa50e729204e9afaf193f28e
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-15 09:42:35 +0530

    Added MongoConnection in Provider

--- src/t2/t2.module.ts
+++ src/t2/t2.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { T2Service } from './t2.service';
 import { T2Controller } from './t2.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { MongoConnectios } from './connection/t2.connect';
 
 @Module({
   imports:[JwtModule.register({
     global: true,
     secret: "dasdsaddad",
     signOptions: { expiresIn: '60s' },
-  }),],
-  providers: [T2Service],
+  })],
+  providers: [T2Service,MongoConnectios],
   controllers:[T2Controller],
   exports:[T2Service]
 })
