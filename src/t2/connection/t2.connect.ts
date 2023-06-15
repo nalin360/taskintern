304e28c77393e160d4b3e94035b7bae25987942a
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-15 09:40:17 +0530

    mongodb connection ,inject in t2.service

--- src/t2/connection/t2.connect.ts
+++ src/t2/connection/t2.connect.ts
@@ -1,16 +1,23 @@
+import { Injectable } from '@nestjs/common';
 import { MongoClient, Db } from 'mongodb';
 
-
+@Injectable()
 export class MongoConnectios{
 
-    async connections(): Promise<any> {
+    async connections(): Promise<Db> {
         // Connect to the MongoDB instance at mongodb://127.0.0.1:27017/
-        const client = await MongoClient.connect('mongodb://127.0.0.1:27017/');
+        try {
+            const client = await MongoClient.connect('mongodb://127.0.0.1:27017/');
         // Send a ping to confirm a successful connection
         if (client.db("work").command({ ping: 1 })) {
             console.log("Pinged your deployment. You successfully connected to MongoDB!")
 
             return client.db('work')
+        }
+        } catch (error) {
+            console.error("Error connection "+error);
+            ;
+            
         }
     }
         
