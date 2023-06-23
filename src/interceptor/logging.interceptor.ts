5bcbc9a389af8d23db0370532128b6e702d46422
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-23 12:13:10 +0530

    added meathod, url, headers, body, query

--- src/interceptor/logging.interceptor.ts
+++ src/interceptor/logging.interceptor.ts
@@ -1,21 +1,29 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger, Header, Query } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-
 @Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+    private readonly logger = new Logger(LoggingInterceptor.name);
 
-export class LoggingInterceptor implements NestInterceptor{
-    intercept(context: ExecutionContext, 
-        next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        console.log("Before ");
-        
-        return next
-        .handle()
-        .pipe(
-            tap(()=> console.log("After")
-            )
-        )
-        
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        // Get a reference to the incoming request
+        const request = context.switchToHttp().getRequest();
+        // Extract the HTTP method and URL from the request object
+        const { method, url , headers, body, query } = request;
+
+        const class_name = context.getClass().name;
+        // Call next interceptor in chain and return response as an Observable
+
+        return next.handle().pipe(
+            tap(() => {
+                this.logger.log(
+                    `${method} ${url} | class_name ${class_name} 
+                    | headers ${JSON.stringify(headers)} 
+                    | query ${JSON.stringify(query)} 
+                    | body ${JSON.stringify(body)} `
+                );
+            }),
+        );
     }
 }
