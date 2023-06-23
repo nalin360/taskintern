01635bf6ea61b025cce2f2275f6359b7cb2c8379
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-23 12:14:23 +0530

    added get and post meathod

--- src/interceptor/interceptor.controller.ts
+++ src/interceptor/interceptor.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, UseInterceptors, Header, Body } from '@nestjs/common';
 import { LoggingInterceptor } from './logging.interceptor';
-
+import { InterceptorService } from './interceptor.service';
+// befor
 @Controller('interceptor')
 @UseInterceptors(LoggingInterceptor)
 export class InterceptorController {
 
+    constructor(private interceptorService: InterceptorService) { }
     @Get()
-    getInterceptor(){
-        return "login interceptor "
+
+
+
+    getInterceptor() {
+        return this.interceptorService.getUser()
     }
 
-    @Post()
-    postInterceptors(){
-        return "postInterceptors"
+    @Post(':id')
+    @Header('Content-Type', 'application/json')
+    create(
+        @Param('id') id: string,
+        @Query('param1') param1: string,
+        @Body() createUserDto: any,
+    ): string {
+        console.log('ID:', id);
+        console.log('Param 1:', param1);
+        console.log('Headers:', Header);
+        console.log('Request Body:', createUserDto);
+        return 'User created';
     }
 }
