190370d604d99a53216d5d7e7a5fa2704cf180a9
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-24 17:17:24 +0530

    added studentid ,createdby , createdfor

--- src/feedback/feedback.dto.ts
+++ src/feedback/feedback.dto.ts
@@ -1,26 +1,71 @@
-import {IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateFeedbackDto {
+
+    @IsNotEmpty()
+    @IsNumber()
+    studentId:number;
+
+    @IsNotEmpty()
+    @IsString()
+    createdBy:string;
+
+    @IsNotEmpty()
+    @IsString()
+    createdFor:string;
 
     // Ensure feedbackFor is either 'teacher' or 'student'
     @IsNotEmpty()
     @IsString()
-    @IsEnum(['teacher', 'student'])
+    // @IsEnum(['teacher', 'student'])
     feedbackFor: string;
 
     @IsNotEmpty()
     @IsString()
     feedbackMessage: string;
-    
+
     @IsNotEmpty()
     feedbackDate: Date;
-    
+
     @IsNotEmpty()
     @IsEnum(['gold', 'silver', 'bronze'])
     rating: string;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    receivedFeedback: boolean;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    createdFeedback:   boolean;
+
 }
 
-// export class GetFeedbackDto {
-//   feedbackFor: string;
-//   rating: string;
-// }
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    studentId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEnum(['teacher', 'student'])
+    role: string;
+
+    @IsNotEmpty()
+    @IsString()
+    createdAt: Date;
+}
