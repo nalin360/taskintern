40dfc52bbac65c06ffbbeb8a053453724aa9e2fc
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-24 17:16:40 +0530

    added query to fetch specific data

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, Sse, UseInterceptors } from '@nestjs/common';
-import { CreateFeedbackDto } from './feedback.dto';
+import { Body, Controller, Get, Post, Query, Sse, UseInterceptors } from '@nestjs/common';
+import { CreateFeedbackDto, CreateUserDto } from './feedback.dto';
 import { FeedbackService } from './feedback.service';
 import { FeedbackInterceptor } from './feedback.interceptor';
 import { Observable, from, map } from 'rxjs';
@@ -11,9 +11,11 @@ export class FeedbackController {
     constructor(private readonly feedbackService: FeedbackService) { }
     
     @Sse('stream')
-    stream(): Observable<{ data: any; }> {
+    stream(@Query('studentId') studentId: number): Observable<{ data: any; }> {
          // Convert the result to an Observable using the from function
-        return from(this.feedbackService.getFeedbacks()).pipe(
+        //  console.log(studentId);
+         
+        return from(this.feedbackService.getFeedbacks(studentId)).pipe(
             map(feedback => ({
                 data: feedback,
             })
@@ -24,6 +26,11 @@ export class FeedbackController {
     @Post()
     async createFeedback(@Body() createFeedbackDto: CreateFeedbackDto) {
         return this.feedbackService.createFeedback(createFeedbackDto)
+        // return this.feedbackService.getFeedbacks()
+    }
+    @Post('user')
+    async createUser(@Body() createUserDto: CreateUserDto) {
+        return this.feedbackService.createUser(createUserDto)
         // return this.feedbackService.getFeedbacks()
     }
 }
