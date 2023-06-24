77bcea111dab80f888882024e786851c8735da2a
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-24 17:15:54 +0530

    response interceptor not completed

--- src/feedback/feedback.interceptor.ts
+++ src/feedback/feedback.interceptor.ts
@@ -1,19 +1,41 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler,Logger } from '@nestjs/common';
+// import { json } from 'express';
+// import Flatted from 'flatted';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+// Flatted
 @Injectable()
+export class FeedbackInterceptor implements NestInterceptor {
+    private readonly logger = new Logger(FeedbackInterceptor.name);
+  
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 
-export class FeedbackInterceptor implements NestInterceptor{
 
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse();
+        // const { method, originalUrl, rawHeaders, body, query  } = response;
+        const class_name = context.getClass().name;
+        // const getHandler = context.getHandler().name
+        // const getHandler = context.getHandler()
+        // const flat = Flatted.stringify(response)
+        console.log(response);
+        // console.log(getHandler);S
+        
 
-        const responses = context.switchToHttp().getResponse()
-        return next 
-        .handle()
-        .pipe(
-            tap(() => console.log(`response ${responses.statusCode}`)),
-          );
-    }
-   
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(
+            `
+            | ${response.method} ${response.url} 
+            | class_name ${class_name} 
+            | headers ${response.rawHeaders} 
+            | query ${response.query} 
+            | body ${response.body} 
+            | Response status: ${response.statusCode}
+            `
+        );
+        // console.log(`Response status: ${response.statusCode}`);
+      }),
+    );
+  }
 }
