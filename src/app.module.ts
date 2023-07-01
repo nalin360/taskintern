dc9a9317340ba7eab979a462b01b10c3c2825dd6
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-07-02 00:21:37 +0530

    added loggoLoggingInterceptor,ResponseInterceptor globaly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module ,NestModule, MiddlewareConsumer} from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
@@ -8,20 +8,30 @@ import { T2Module } from './t2/t2.module';
 import { MidwayModule } from './midway/midway.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { InterceptorModule } from './interceptor/interceptor.module';
-import { FeedbackController } from './feedback/feedback.controller';
-import { FeedbackService } from './feedback/feedback.service';
 import { FeedbackModule } from './feedback/feedback.module';
+import { NotificationModule } from './notification/notification.module';
+import { LoggingInterceptor } from './interceptor/logging.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseInterceptor } from './feedback/response.interceptor';
 
 
 
 @Module({
-  imports: [TaskModule , 
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/task'), 
-    UserModule,T2Module,MidwayModule, InterceptorModule, FeedbackModule],
+  imports: [TaskModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/task'),
+    UserModule, T2Module, MidwayModule, InterceptorModule, FeedbackModule, NotificationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor, 
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ResponseInterceptor,
+  },
+],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
