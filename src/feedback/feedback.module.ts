4d08949ed7990c459d6ad8b8b3b99723c2f34282
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-07-02 00:18:02 +0530

    NotificationController,NotificationService

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
 import { MongoConnectios } from 'src/t2/connection/t2.connect';
+import { NotificationController } from 'src/notification/notification.controller';
+import { NotificationModule } from '../notification/notification.module';
+import { NotificationService } from '../notification/notification.service';
 
 @Module({
     imports:[],
     controllers:[FeedbackController],
-    providers:[FeedbackService,MongoConnectios]
+    providers:[FeedbackService,MongoConnectios,NotificationController,NotificationService]
 })
 export class FeedbackModule {}
