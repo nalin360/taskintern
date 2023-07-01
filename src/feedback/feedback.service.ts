fef6e2f95c74269b2c5f800c5376f7d85c665118
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-07-02 00:17:18 +0530

    meathod to create feedbak, insertedDocument

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,64 +1,43 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MongoConnectios } from 'src/t2/connection/t2.connect';
 import { CreateFeedbackDto } from './feedback.dto';
+import { NotificationController } from 'src/notification/notification.controller';
 
 @Injectable()
 export class FeedbackService {
     constructor(
-        @Inject(MongoConnectios) private mongoconnect: MongoConnectios
+        @Inject(MongoConnectios) private mongoconnect: MongoConnectios,
+        private notificationController: NotificationController
     ) { }
-    notifications() {
-        return "notifications";
-    }
 
-    async getFeedbacks(studentId: number) {
-        const users = await this.mongoconnect.connections()
-        // const getfeedback = await users.collection("feedback").find().sort({_id:-1}).limit(1).toArray()
-        // Collection.find(query, projection);
-        // query - query for filtering out the data
-        // projection - Specifies the fields to return in the documents that match the query filter
-        console.log(studentId);
-
-        const pipeline = [
-            {
-                $match: { studentId: studentId }
-            },
-            {
-                $lookup: {
-                    // studentId`, which exists in the current collection
-                    from: 'feedbacks', localField: 'studentId',
-                    // foreign field is also `studentId`, which exists in the `feedbacks` collection.
-                    foreignField: 'studentId', as: 'feedbacks'
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    studentId: '$feedbacks.studentId',
-                    createdFor: '$feedbacks.createdFor',
-                    feedbackMessage: '$feedbacks.feedbackMessage'
-                }
-            }
-
-        ];
-
-        const getfeedback = await users
-            .collection('users').aggregate(pipeline).toArray();
-
-        console.log(getfeedback);
-        return getfeedback;
-    }
     async createFeedback(createFeedbackDto: CreateFeedbackDto) {
         const users = await this.mongoconnect.connections()
         // Get the 'users' collection from the database
         const feedbackCreated = await users.collection("feedback").insertOne(createFeedbackDto)
-        return feedbackCreated;
+        
+        const insertedDocument = await users.collection("feedback")
+            .findOne({ _id: feedbackCreated.insertedId });
+        // Retrieve the studentId from the inserted document
+        const studentId = insertedDocument.studentId;
+
+        const notification = this.notificationController.stream(studentId)
+        // return notification;
+        return notification
     }
 
     async createUser(createUserkDto: any) {
         const users = await this.mongoconnect.connections()
         // Get the 'users' collection from the database
-        const feedbackCreated = await users.collection("feedback").insertOne(createUserkDto)
+        const feedbackCreated = await users.collection("users").insertOne(createUserkDto)
         return feedbackCreated;
+    }
+    async findFeedback():Promise<object>{
+        const users = await this.mongoconnect.connections()
+        // Get the 'users' collection from the database
+        const findId = await users.collection("feedback").find().toArray()
+        // console.log(findId);
+        // console.log("findFeedback ..........................");
+        
+        return findId;
     }
 }
