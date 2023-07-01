82ce0b6f84e85752f7571698fdbd6a35ec6e3135
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-07-02 00:19:05 +0530

    removed sse implementation and get meathod to get all the feed back data

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,36 +1,33 @@
 import { Body, Controller, Get, Post, Query, Sse, UseInterceptors } from '@nestjs/common';
 import { CreateFeedbackDto, CreateUserDto } from './feedback.dto';
 import { FeedbackService } from './feedback.service';
-import { FeedbackInterceptor } from './feedback.interceptor';
 import { Observable, from, map } from 'rxjs';
+import { ResponseInterceptor } from './response.interceptor';
 
 @Controller('feedback')
-@UseInterceptors(FeedbackInterceptor)
+// @UseInterceptors(ResponseInterceptor)
 export class FeedbackController {
 
     constructor(private readonly feedbackService: FeedbackService) { }
-    
-    @Sse('stream')
-    stream(@Query('studentId') studentId: number): Observable<{ data: any; }> {
-         // Convert the result to an Observable using the from function
-        //  console.log(studentId);
-         
-        return from(this.feedbackService.getFeedbacks(studentId)).pipe(
-            map(feedback => ({
-                data: feedback,
-            })
-            )
-        )
-    }
+
+
 
     @Post()
     async createFeedback(@Body() createFeedbackDto: CreateFeedbackDto) {
         return this.feedbackService.createFeedback(createFeedbackDto)
         // return this.feedbackService.getFeedbacks()
     }
+
     @Post('user')
     async createUser(@Body() createUserDto: CreateUserDto) {
         return this.feedbackService.createUser(createUserDto)
         // return this.feedbackService.getFeedbacks()
     }
+
+
+    @Get()
+    async findFeedBAck() {
+        return this.feedbackService.findFeedback()
+    }
+
 }
