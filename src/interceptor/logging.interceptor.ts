0e2a9d97a97d1aa7296349e0e7a25996337d61ed
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-07-02 00:20:03 +0530

    loging interceptor in object fromate

--- src/interceptor/logging.interceptor.ts
+++ src/interceptor/logging.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger, Header, Query } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -10,20 +10,43 @@ export class LoggingInterceptor implements NestInterceptor {
         // Get a reference to the incoming request
         const request = context.switchToHttp().getRequest();
         // Extract the HTTP method and URL from the request object
-        const { method, url , headers, body, query } = request;
+        const { method, url, headers, body, query } = request;
+
+        // console.log(request);
 
         const class_name = context.getClass().name;
         // Call next interceptor in chain and return response as an Observable
-
-        return next.handle().pipe(
-            tap(() => {
-                this.logger.log(
-                    `${method} ${url} | class_name ${class_name} 
-                    | headers ${JSON.stringify(headers)} 
-                    | query ${JSON.stringify(query)} 
-                    | body ${JSON.stringify(body)} `
-                );
-            }),
-        );
+        const now = Date.now();
+        // https://socket.dev/npm/package/@algoan/nestjs-logging-interceptor
+        return next.handle().pipe
+            (
+                map((data) => {
+                    const log = {
+                        message: `${LoggingInterceptor.name} - method: {method} -  url: ${url}`,
+                        class_name:`${class_name}`,
+                        body: body,
+                        headers: headers,
+                    };
+                    Logger.log(log)
+                    // console.log('After...');
+                    return data;
+                }
+                    
+                )
+            )
     }
 }
+
+// `${method} ${url} ${Date.now() - now}ms`,
+//                         `headers ${JSON.stringify(headers)}`,
+//                         `query ${JSON.stringify(query)} `,
+//                         `body ${JSON.stringify(body)}`,
+//                         `${class_name}`,
+// tap(() => {
+                    //     this.logger.log(
+                    //         `${method} ${url} | class_name ${class_name} 
+                    //         |  
+                    //         | 
+                    //         |  `
+                    //     );
+                    // }),
